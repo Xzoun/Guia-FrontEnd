968894814c16a11d5eff8dec670d79b42bfc8371
Leo Benitez <L96@live.com.ar>
2023-09-08 22:08:58 -0300

    Actualizacion

--- JavaScript/Ejercicios/JavaScript/Ejercicio 24.js
+++ JavaScript/Ejercicios/JavaScript/Ejercicio 24.js
@@ -1,28 +1,69 @@
 const canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
     resolverBtn = document.getElementById("resolver"),
-    diametro = document.getElementById("diametro"),
+    datoHTML = document.getElementById("dato"),
+    valorHTML = document.getElementById("valor"),
     outPut = document.getElementById("salida"),
-    container = document.getElementById("canvasCont");
+    container = document.getElementById("canvasCont"),
+    pi = Math.PI;;
 
 resolverBtn.addEventListener("click", () => {
+    let dato = datoHTML.value,
+        valor = valorHTML.value,
+        verdadero = true,
+        falso = false,
+        radio = 0,
+        area = 0,
+        perimetro = 0;
 
-    if (diametro.value > 1000) {
-        alert("El diametro debe ser menor a 1000. Lo sentimos.")
-    } else {
-        limpiarCanvas();
-        outPut.innerText = "";
+    switch (dato) {
+        case "area":
+            radio = Math.sqrt(valor / Math.PI);
+            falso = false;
+            break;
+        case "perimetro":
+            radio = valor / (2 * Math.PI);
+            falso = false;
+            break;
+        case "diametro":
+            radio = valor / 2;
+            falso = false;
+            break;
+        case "radio":
+            verdadero = false;
+            perimetro = 2 * pi * valor;
+            area = pi * valor * valor;
+            radio = valor;
+            falso = false;
+            break;
+        default:
+            alert("Debes seleccionar una opción.")
+            falso = true;
+            break;
+    }
+    if (!falso) {
+        if (verdadero) {
+            limpiarCanvas();
 
-        let radio = diametro.value / 2;
+            ctx.beginPath();
+            ctx.arc(container.clientWidth / 2, radio, radio, 0, 2 * Math.PI);
+            ctx.fillStyle = "#F5B041";
 
-        ctx.beginPath();
-        ctx.arc(container.clientWidth/2, radio, radio, 0, 2 * Math.PI);
-        ctx.fillStyle = "#F5B041";
+            ctx.fill();
+            ctx.closePath();
+            outPut.innerText = "Radio: " + radio;
 
-        ctx.fill();
-        ctx.closePath();
+        } else {
+            limpiarCanvas();
 
-        outPut.innerText = "Radio: " + radio;
+            ctx.beginPath();
+            ctx.arc(container.clientWidth / 2, radio, radio, 0, 2 * Math.PI);
+            ctx.fillStyle = "#F5B041";
+
+            ctx.fill();
+            ctx.closePath();
+            outPut.innerText = "Área: " + area + "\n" + "Perímetro: " + perimetro;
+        }
     }
 });
 
