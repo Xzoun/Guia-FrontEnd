24870fe56d5c93a7d7cfd86c1dd87126fab88912
Leo Benitez <L96@live.com.ar>
2023-09-20 14:13:56 -0300

    Actualizacion

--- Imagenes/JavaScript/Ejercicios/JavaScript/Ejercicio 1.js
+++
@@ -1,8 +0,0 @@
-const lista = document.getElementById("lista"),
-    outPut = document.getElementById("salida");
-
-lista.addEventListener("click", (e) => {
-    let clima = e.target.getAttribute("data-Clima");
-    document.getElementById("respuesta").style.display = "block";
-    outPut.innerText = clima;
-})
